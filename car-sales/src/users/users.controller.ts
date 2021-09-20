ac92c780c7651d268030a6e490c70021bf91b038
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2021-09-20 19:12:44 +0600

    two controllers added

--- car-sales/src/users/users.controller.ts
+++ car-sales/src/users/users.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { FetchUsersDto } from './dtos/findAllUser.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+  async createUser(@Body() body: CreateUserDto) {
+    const users = await this.userService.fetchUsers(body.email);
+    const usersEmail = [];
+    users.forEach((user) => usersEmail.push(user.email));
+    if (usersEmail.includes(body.email)) {
+      return 'Duplicate Email, Please use another email';
+    } else this.userService.createsUser(body.email, body.password);
+  }
+
+  @Get('/all')
+  async fetchAllUsers(@Body() body: FetchUsersDto) {
+    const users = await this.userService.fetchUsers(body.email);
+    return users;
   }
 }
--- car-sales/src/users/users.service.ts
+++ car-sales/src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  createsUser(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  fetchUsers(email: string) {
+    return this.repo.find({ email });
+  }
+}
